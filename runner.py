e94508f33e9f3b7e21ce8b9345297173ca48f0bf
Cassidy Armstrong <carmstrong1@gmail.com>
2023-07-10 15:10:57 +0100

    first commit

---
+++ runner.py
@@ -0,0 +1,13 @@
+
+from students import Student 
+
+cass = Student("Cassidy", "e65")
+stuart = Student("Stuart", "E65")
+
+print(cass.holder_name)
+
+
+
+
+
+
---
+++ students.py
@@ -0,0 +1,16 @@
+class Student:
+
+    def __init__(self, input_holder_name, cohort_num):
+        self.holder_name = input_holder_name 
+        self.cohort = cohort_num
+    
+    def talk(self):
+        return "I can talk"
+    
+    def say_favourite_language(self):
+        return "I love Python!"
+
+
+
+
+
